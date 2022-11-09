2b8dd96f14f878b1500076ff04442e53729f79b1
crdgdr <q_li@ucsb.edu>
2022-11-08 17:02:27 -0800

    vl - added nav bar and nav bar test for articles

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -75,6 +75,14 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                   </NavDropdown>
                 )
               }
+              {
+                hasRole(currentUser, "ROLE_USER") && (
+                  <NavDropdown title="Articles" id="appnavbar-articles-dropdown" data-testid="appnavbar-articles-dropdown" >
+                    <NavDropdown.Item as={Link} to="/articles/list">List Articles</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/articles/create">Create Article</NavDropdown.Item>
+                  </NavDropdown>
+                )
+              }
             </Nav>
 
             <Nav className="ml-auto">
