9759a31e1c6b6ec47535ff2a28c70b83916076cd
Zackary Glazewski <zglazewski@umail.ucsb.edu>
2022-11-10 17:41:15 -0800

    ZG - adds delete implementation and testing

--- frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
@@ -1,10 +1,20 @@
-import OurTable from "main/components/OurTable";
-// import { useBackendMutation } from "main/utils/useBackend";
-// import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBDateUtils"
-// import { useNavigate } from "react-router-dom";
-//import { hasRole } from "main/utils/currentUser";
+import OurTable, { ButtonColumn} from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import { _useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
 
-export default function UCSBOrganizationTable({ ucsbOrganizations, _currentUser }) {
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/UCSBOrganization",
+        method: "DELETE",
+        params: {
+            orgCode: cell.row.values.orgCode
+        }
+    }
+}
+
+export default function UCSBOrganizationTable({ ucsbOrganizations, currentUser }) {
 
     // const navigate = useNavigate();
 
@@ -12,16 +22,16 @@ export default function UCSBOrganizationTable({ ucsbOrganizations, _currentUser 
     //     navigate(`/ucsbdates/edit/${cell.row.values.id}`)
     // }
 
-    // // Stryker disable all : hard to test for query caching
-    // const deleteMutation = useBackendMutation(
-    //     cellToAxiosParamsDelete,
-    //     { onSuccess: onDeleteSuccess },
-    //     ["/api/ucsbdates/all"]
-    // );
-    // // Stryker enable all 
+    // Stryker disable all : hard to test for query caching
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/UCSBOrganization/all"]
+    );
+    // Stryker enable all 
 
-    // // Stryker disable next-line all : TODO try to make a good test for this
-    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    // Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
     const columns = [
         {
@@ -38,18 +48,18 @@ export default function UCSBOrganizationTable({ ucsbOrganizations, _currentUser 
         },
         {
             Header: 'Inactive?',
+            id: 'inactive',
             accessor: (row, _rowIndex) => String(row.inactive)
         }
     ];
 
-    // const columnsIfAdmin = [
-    //     ...columns,
-    //     ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
-    //     ButtonColumn("Delete", "danger", deleteCallback, "UCSBDatesTable")
-    // ];
+    const columnsIfAdmin = [
+        ...columns,
+        //ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
+        ButtonColumn("Delete", "danger", deleteCallback, "UCSBOrganizationTable")
+    ];
 
-    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
-    const columnsToDisplay = columns;
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
 
     return <OurTable
         data={ucsbOrganizations}
