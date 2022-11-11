a126ac9f67de7faf696c38d4730b939a48145c44
Sitian Qiu <sitian@umail.ucsb.edu>
2022-11-10 17:58:20 -0800

    TQ - Add RecommendationTable and Table.stories.js

--- frontend/src/tests/pages/Recommendation/RecommendationIndexPage.test.js
+++ frontend/src/tests/pages/Recommendation/RecommendationIndexPage.test.js
@@ -1,4 +1,5 @@
-import { fireEvent, render, waitFor } from "@testing-library/react";
+// import { fireEvent, render, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import RecommendationIndexPage from "main/pages/Recommendation/RecommendationIndexPage";
@@ -6,10 +7,10 @@ import RecommendationIndexPage from "main/pages/Recommendation/RecommendationInd
 
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import { recommendationFixtures } from "fixtures/recommendationFixtures";
+// import { recommendationFixtures } from "fixtures/recommendationFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import mockConsole from "jest-mock-console";
+// import mockConsole from "jest-mock-console";
 
 
 const mockToast = jest.fn();
@@ -26,7 +27,7 @@ describe("RecommendationIndexPage tests", () => {
 
     const axiosMock =new AxiosMockAdapter(axios);
 
-    const testId = "RecommendationTable";
+    // const testId = "RecommendationTable";
 
     const setupUserOnly = () => {
         axiosMock.reset();
