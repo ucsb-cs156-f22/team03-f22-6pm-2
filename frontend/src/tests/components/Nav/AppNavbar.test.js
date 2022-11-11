e39eef9c35284e8105ab138a897d5702aef03ffc
christianvanstralen <72837831+christianvanstralen@users.noreply.github.com>
2022-11-10 20:05:16 -0800

    CVS - Replaced place holder page with MenuItemReviewTable (#63)

--- frontend/src/tests/components/Nav/AppNavbar.test.js
+++ frontend/src/tests/components/Nav/AppNavbar.test.js
@@ -82,15 +82,15 @@ describe("AppNavbar tests", () => {
 
         await waitFor(() => expect(getByTestId("appnavbar-todos-dropdown")).toBeInTheDocument());
     });
-    
-    test("renders the MenuItemReview menu correctly", async () => {
+
+    test("renders the MenuItemReview menu correctly for a user", async () => {
 
         const currentUser = currentUserFixtures.userOnly;
         const systemInfo = systemInfoFixtures.showingBoth;
 
         const doLogin = jest.fn();
 
-        const {getByTestId } = render(
+        const {getByTestId  } = render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
                     <AppNavbar currentUser={currentUser} systemInfo={systemInfo} doLogin={doLogin} />
@@ -99,9 +99,40 @@ describe("AppNavbar tests", () => {
         );
 
         await waitFor(() => expect(getByTestId("appnavbar-menuitemreviews-dropdown")).toBeInTheDocument());
+        const dropdown = getByTestId("appnavbar-menuitemreviews-dropdown");
+        const aElement = dropdown.querySelector("a");
+        expect(aElement).toBeInTheDocument();
+        aElement?.click();
+        await waitFor( () => expect(getByTestId("appnavbar-menuitemreviews-list")).toBeInTheDocument() );
+
     });
 
+    // test("renders the MenuItemReview menu correctly for an admin", async () => {
+
+    //     const currentUser = currentUserFixtures.adminUser;
+    //     const systemInfo = systemInfoFixtures.showingBoth;
+
+    //     const doLogin = jest.fn();
+
+    //     const {getByTestId  } = render(
+    //         <QueryClientProvider client={queryClient}>
+    //             <MemoryRouter>
+    //                 <AppNavbar currentUser={currentUser} systemInfo={systemInfo} doLogin={doLogin} />
+    //             </MemoryRouter>
+    //         </QueryClientProvider>
+    //     );
+
+    //     await waitFor(() => expect(getByTestId("appnavbar-menuitemreviews-dropdown")).toBeInTheDocument());
+    //     const dropdown = getByTestId("appnavbar-menuitemreviews-dropdown");
+    //     const aElement = dropdown.querySelector("a");
+    //     expect(aElement).toBeInTheDocument();
+    //     aElement?.click();
+    //     await waitFor( () => expect(getByTestId(/appnavbar-menuitemreviews-create/)).toBeInTheDocument() );
+
+    // });
+
     test("renders the UCSBOrganization menu correctly for a user", async () => {
+
         const currentUser = currentUserFixtures.userOnly;
         const systemInfo = systemInfoFixtures.showingBoth;
 
