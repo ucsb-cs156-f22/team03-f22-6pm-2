2371469f9f1568c12dc8367386a57d22a833f539
BrianOzburns <73633726+BrianOzburns@users.noreply.github.com>
2022-11-11 23:13:41 -0800

    BOB - added delete functionality and appropriate tests (#73)
    * BOB - added delete functionality and appropriate tests
    
    * BOB - fixed delete stryker test
    
    Co-authored-by: Brian Ozawa Burns <BrianOzburns@gmail.com>

--- frontend/src/tests/pages/MenuItems/MenuItemsIndexPage.test.js
+++ frontend/src/tests/pages/MenuItems/MenuItemsIndexPage.test.js
@@ -1,4 +1,4 @@
-import { _fireEvent, render, waitFor } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import MenuItemsIndexPage from "main/pages/MenuItems/MenuItemsIndexPage";
@@ -134,35 +134,35 @@ describe("MenuItemsIndexPage tests", () => {
         expect(queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
     });
 
-    // test("test what happens when you click delete, admin", async () => {
-    //     setupAdminUser();
+    test("test what happens when you click delete, admin", async () => {
+        setupAdminUser();
 
-    //     const queryClient = new QueryClient();
-    //     axiosMock.onGet("/api/UCSBDiningCommonsMenuItem/all").reply(200, menuItemsFixtures.threeMenuItems);
-    //     axiosMock.onDelete("/api/UCSBDiningCommonsMenuItem").reply(200, "MenuItem with id 1 was deleted");
-
-
-    //     const { getByTestId } = render(
-    //         <QueryClientProvider client={queryClient}>
-    //             <MemoryRouter>
-    //                 <MenuItemsIndexPage />
-    //             </MemoryRouter>
-    //         </QueryClientProvider>
-    //     );
-
-    //     await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toBeInTheDocument(); });
-
-    //    expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1"); 
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/UCSBDiningCommonsMenuItem/all").reply(200, menuItemsFixtures.threeMenuItems);
+        axiosMock.onDelete("/api/UCSBDiningCommonsMenuItem", {params: {id: 1}}).reply(200, "MenuItem with id 1 was deleted");
 
 
-    //     const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
-    //     expect(deleteButton).toBeInTheDocument();
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <MenuItemsIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toBeInTheDocument(); });
+
+       expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1"); 
+
+
+        const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+        expect(deleteButton).toBeInTheDocument();
        
-    //     fireEvent.click(deleteButton);
+        fireEvent.click(deleteButton);
 
-    //     await waitFor(() => { expect(mockToast).toBeCalledWith("MenuItem with id 1 was deleted") });
+        await waitFor(() => { expect(mockToast).toBeCalledWith("MenuItem with id 1 was deleted") });
 
-    // });
+    });
 
 });
 
